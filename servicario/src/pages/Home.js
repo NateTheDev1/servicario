ad2cc904da93f4e112112659426887198195d41c
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-01 02:15:01 -0400

    Refactor to ServiceItem.js and Hero.js

--- servicario/src/pages/Home.js
+++ servicario/src/pages/Home.js
@@ -4,6 +4,8 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import NavbarClone from "../components/NavbarClone";
 import { getServices } from "../store/index";
+import ServiceItem from "../components/service/ServiceItem";
+import Hero from "../components/Hero";
 
 class Home extends React.Component {
   state = {
@@ -16,29 +18,7 @@ class Home extends React.Component {
   }
 
   renderServices = (services) => {
-    return services.map((s) => (
-      <div className="column is-one-third" key={s.id}>
-        <div
-          className="feature-card is-bordered has-text-centered revealOnScroll delay-1"
-          data-animation="fadeInLeft"
-        >
-          <div className="card-title">
-            <h4>{s.title}</h4>
-          </div>
-          <div className="card-icon">
-            <img src={s.image} alt="" />
-          </div>
-          <div className="card-text">
-            <p>{s.description}</p>
-          </div>
-          <div className="card-action">
-            <a href="#" className="button btn-align-md accent-btn raised">
-              Learn More
-            </a>
-          </div>
-        </div>
-      </div>
-    ));
+    return services.map((s) => <ServiceItem service={s} key={s.id} />);
   };
 
   render() {
@@ -47,37 +27,7 @@ class Home extends React.Component {
       <div>
         <Navbar />
         <NavbarClone />
-        <section className="hero is-default is-bold">
-          <div className="hero-body">
-            <div className="container">
-              <div className="columns is-vcentered">
-                <div className="column is-5 is-offset-1 landing-caption">
-                  <h1 className="title is-1 is-bold is-spaced">
-                    Learn, Collaborate.
-                  </h1>
-                  <h2 className="subtitle is-5 is-muted">
-                    Lorem ipsum sit dolor amet is a dummy text used by
-                    typography industry{" "}
-                  </h2>
-                  <p>
-                    <a className="button cta rounded primary-btn raised">
-                      Get Started
-                    </a>
-                  </p>
-                </div>
-                <div className="column is-5 is-offset-1">
-                  <figure className="image is-4by3">
-                    <img
-                      src={process.env.PUBLIC_URL + "/worker.svg"}
-                      alt="Description"
-                    />
-                  </figure>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
+        <Hero />
         <section className="section section-feature-grey is-medium">
           <div className="container">
             <div className="title-wrapper has-text-centered">
