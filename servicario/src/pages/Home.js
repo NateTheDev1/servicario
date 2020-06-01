dfb67ef971b3434b5a3b64708e3c802d04eae6ec
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-01 01:18:20 -0400

    Home Page Started

---
+++ servicario/src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Home.css";
+
+// const Home = () => {
+//   return (
+//     <div className="container">
+//       <h1>I am Home Page</h1>
+//     </div>
+//   );
+// };
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>I am Home Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
