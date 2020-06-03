08505b05d28f271bac39989c9747c80041d20a26
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-03 11:57:34 -0400

    Sent and received offers pages working

--- servicario/src/pages/offers/ReceivedOffers.js
+++ servicario/src/pages/offers/ReceivedOffers.js
@@ -1,40 +1,49 @@
 import React from "react";
 import withAuthorization from "../../components/hoc/withAuthorization";
 import ServiceItem from "../../components/service/ServiceItem";
+import { fetchReceivedOffers } from "actions";
+import { connect } from "react-redux";
 
 class ReceivedOffers extends React.Component {
+  componentDidMount() {
+    const { auth } = this.props;
+    this.props.dispatch(fetchReceivedOffers(auth.user.uid));
+  }
+
   render() {
+    const { offers } = this.props;
     return (
       <div className="container">
         <div className="content-wrapper">
           <h1 className="title">Received Offers</h1>
           <div className="columns">
-            <div className="column is-one-third">
-              {/* <ServiceItem
-              noButton
-              className="offer-card"
-              service={o.service}>
-              <div className="tag is-large">
-                {o.status}
+            {offers.map((o) => (
+              <div className="column is-one-third">
+                <ServiceItem
+                  noButton
+                  className="offer-card"
+                  service={o.service}
+                >
+                  <div className="tag is-large">{o.status}</div>
+                  <hr />
+                  <div className="service-offer">
+                    <div>
+                      <span className="label">From User:</span>
+                      {o.fromUser.fullName}
+                    </div>
+                    <div>
+                      <span className="label">Note:</span> {o.note}
+                    </div>
+                    <div>
+                      <span className="label">Price:</span> ${o.price}
+                    </div>
+                    <div>
+                      <span className="label">Time:</span> {o.time} hours
+                    </div>
+                  </div>
+                </ServiceItem>
               </div>
-              <hr />
-              <div className="service-offer">
-                <div>
-                  <span className="label">From User:</span> {o.toUser.fullName}
-                </div>
-                <div>
-                  <span className="label">Note:</span> {o.note}
-                </div>
-                <div>
-                  <span className="label">Price:</span> ${o.price}
-                </div>
-                <div>
-                  <span className="label">Time:</span> {o.time} hours
-                </div>
-              </div>
-            </ServiceItem>
-            */}
-            </div>
+            ))}
           </div>
         </div>
       </div>
@@ -42,4 +51,6 @@ class ReceivedOffers extends React.Component {
   }
 }
 
-export default withAuthorization(ReceivedOffers);
+const mapStateToProps = (state) => ({ offers: state.offers.received });
+
+export default withAuthorization(connect(mapStateToProps)(ReceivedOffers));
--- servicario/src/pages/offers/SentOffers.js
+++ servicario/src/pages/offers/SentOffers.js
@@ -1,40 +1,49 @@
 import React from "react";
 import withAuthorization from "../../components/hoc/withAuthorization";
 import ServiceItem from "../../components/service/ServiceItem";
+import { fetchSentOffers } from "actions";
+import { connect } from "react-redux";
 
 class SentOffers extends React.Component {
+  componentDidMount() {
+    const { auth } = this.props;
+    this.props.dispatch(fetchSentOffers(auth.user.uid));
+  }
+
   render() {
+    const { offers } = this.props;
     return (
       <div className="container">
         <div className="content-wrapper">
           <h1 className="title">Sent Offers</h1>
           <div className="columns">
-            <div className="column is-one-third">
-              {/* <ServiceItem
-              noButton
-              className="offer-card"
-              service={o.service}>
-              <div className="tag is-large">
-                {o.status}
+            {offers.map((o) => (
+              <div className="column is-one-third">
+                <ServiceItem
+                  noButton
+                  className="offer-card"
+                  service={o.service}
+                >
+                  <div className="tag is-large">{o.status}</div>
+                  <hr />
+                  <div className="service-offer">
+                    <div>
+                      <span className="label">To User:</span>{" "}
+                      {o.toUser.fullName}
+                    </div>
+                    <div>
+                      <span className="label">Note:</span> {o.note}
+                    </div>
+                    <div>
+                      <span className="label">Price:</span> ${o.price}
+                    </div>
+                    <div>
+                      <span className="label">Time:</span> {o.time} hours
+                    </div>
+                  </div>
+                </ServiceItem>
               </div>
-              <hr />
-              <div className="service-offer">
-                <div>
-                  <span className="label">From User:</span> {o.toUser.fullName}
-                </div>
-                <div>
-                  <span className="label">Note:</span> {o.note}
-                </div>
-                <div>
-                  <span className="label">Price:</span> ${o.price}
-                </div>
-                <div>
-                  <span className="label">Time:</span> {o.time} hours
-                </div>
-              </div>
-            </ServiceItem>
-            */}
-            </div>
+            ))}
           </div>
         </div>
       </div>
@@ -42,4 +51,6 @@ class SentOffers extends React.Component {
   }
 }
 
-export default withAuthorization(SentOffers);
+const mapStateToProps = (state) => ({ offers: state.offers.sent });
+
+export default withAuthorization(connect(mapStateToProps)(SentOffers));
